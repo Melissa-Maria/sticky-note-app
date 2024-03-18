76701ec08f34f4d990718ebb3d4248f01d6f5fb8
Melissa Pignalosa <m.pignalosa@hotmail.com>
2024-03-18 17:54:42 +0800

    Assigned data to components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header";
 import NotesList from "./NotesList";
 
-const App = () => (
-  <div>
-    <Header />
-    <NotesList />
-  </div>
-);
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: 0,
+        title: "eat",
+        description: "reese peanut butter cups",
+        doesMatchSearch: true
+      },
+      {
+        id: 1,
+        title: "sleep",
+        description: "eight hours",
+        doesMatchSearch: true
+      },
+      {
+        id: 2,
+        title: "code",
+        description: "build an awesome ui",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: "Search for me"
+  };
+  render() {
+    return (
+      <div>
+        <Header />
+        <NotesList />
+      </div>
+    );
+  }
+}
 
 export default App;
